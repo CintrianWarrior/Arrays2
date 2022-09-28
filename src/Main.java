6a4ff5aef57b75aa4188dea6ad48c339eea69f7d
Cintrian <velegoj2@yandex.ru>
2022-09-28 22:06:00 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int [] dailyExpenses = generateRandomArray();
-        int maxDailyExpenses = -1;
-        int minDailyExpenses = 200_001;
+        int total = 0;
         for (int i = 0; i < dailyExpenses.length; i++) {
-            System.out.println(dailyExpenses[i]);
-            if (dailyExpenses[i] > maxDailyExpenses) {
-                maxDailyExpenses = dailyExpenses[i];
-            }
-            if (dailyExpenses[i] < minDailyExpenses) {
-                minDailyExpenses = dailyExpenses[i];
-            }
+            total = total + dailyExpenses[i];
         }
-        System.out.println("Минимальная сумма трат за день составила " + minDailyExpenses + " рублей. Максимальная сумма трат за день составила " + maxDailyExpenses + " рублей.");
+        double averageDailyExpenses = (total / dailyExpenses.length);
+        System.out.println("Средняя сумма трат за месяц составила " + averageDailyExpenses + " рублей.");
     }
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
