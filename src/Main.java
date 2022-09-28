f0c04af9f05790735aef6ce0d9e460b57de5d539
Cintrian <velegoj2@yandex.ru>
2022-09-28 22:21:18 +0300

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int [] dailyExpenses = generateRandomArray();
-        int total = 0;
-        for (int i = 0; i < dailyExpenses.length; i++) {
-            total = total + dailyExpenses[i];
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
-        double averageDailyExpenses = (total / dailyExpenses.length);
-        System.out.println("Средняя сумма трат за месяц составила " + averageDailyExpenses + " рублей.");
-    }
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-        }
-        return arr;
     }
     }
