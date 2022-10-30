b9898d533f611833e7555dbedb420239e5446784
CintrianWarrior <velegoj2@yandex.ru>
2022-10-30 17:41:43 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,8 @@ public class Main {
         for (int i = 0; i < dailyExpenses.length; i++) {
             total = total + dailyExpenses[i];
         }
-        int averageDailyExpenses = total / dailyExpenses.length;
-        int averageDailyExpenses2 = total%dailyExpenses.length;
-        System.out.println("Средняя сумма трат за месяц составила " + averageDailyExpenses + "," +averageDailyExpenses2 + " рублей.");
+        double averageDailyExpenses = (double) total / dailyExpenses.length;
+        System.out.println("Средняя сумма трат за месяц составила " + averageDailyExpenses + " рублей.");
     }
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
